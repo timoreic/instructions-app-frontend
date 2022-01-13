d224398c1a18f4919f89c8d71a99418d607708ea
Timo <t.reichelt@live.de>
2022-01-13 12:29:34 +0800

    Connected API for Instructions and Instruction components

--- src/components/Instruction.tsx
+++ src/components/Instruction.tsx
@@ -1,42 +1,88 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
+type InstructionType = {
+  id: number;
+  title: string;
+  description: string;
+  rating: number;
+  categories: string[];
+  steps: string[];
+};
+
 export default function Instruction() {
   const { id } = useParams();
-  let [instruction, setInstruction] = useState({
-    id: 0,
-    title: '',
-    time: 0,
-  });
+  let [instruction, setInstruction] = useState({} as InstructionType);
+  let [error, setError] = useState(0);
+  let [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(
-    () => setInstruction({ id: 0, title: 'Some Instruction', time: 150 }),
-    [instruction]
-  );
+  useEffect(() => {
+    fetch(`http://localhost:4000/v1/instruction/${id}`)
+      .then((response) => {
+        console.log('status code is: ', response.status);
+        if (response.status !== 200) {
+          setError(response.status);
+        } else {
+          setError(0);
+        }
+        return response.json();
+      })
+      .then((json) => setInstruction(json.instruction))
+      .then(() => setIsLoaded(true));
+  }, [id]);
 
-  return (
-    <Fragment>
-      <h2>
-        Instruction {id}: {instruction.title}
-      </h2>
+  if (instruction.categories) {
+    instruction.categories = Object.values(instruction.categories);
+  } else {
+    instruction.categories = [];
+  }
 
-      <table className="table table-compact table-striped">
-        <thead></thead>
-        <tbody>
-          <tr>
-            <td>
-              <strong>Title:</strong>
-            </td>
-            <td>{instruction.title}</td>
-          </tr>
-          <tr>
-            <td>
-              <strong>Run time:</strong>
-            </td>
-            <td>{instruction.time} minutes</td>
-          </tr>
-        </tbody>
-      </table>
-    </Fragment>
-  );
+  if (error !== 0) {
+    return <h2>Error: {error}</h2>;
+  } else if (isLoaded !== true) {
+    return <h2>Loading...</h2>;
+  } else {
+    return (
+      <Fragment>
+        <h2>{instruction.title}</h2>
+
+        <div className="float-start">
+          <small>Rating: {instruction.rating}</small>
+        </div>
+
+        <div className="float-end">
+          <small>
+            {instruction.categories.map((m, index) => (
+              <span className="badge bg-secondary me-1" key={index}>
+                {m}
+              </span>
+            ))}
+          </small>
+        </div>
+        <div className="clearfix"></div>
+
+        <hr />
+
+        <table className="table table-compact table-striped">
+          <thead></thead>
+
+          <tbody>
+            <tr>
+              <td>
+                <strong>Description:</strong>
+              </td>
+              <td>{instruction.description}</td>
+            </tr>
+
+            <tr>
+              <td>
+                <strong>Steps:</strong>
+              </td>
+              <td>{instruction.steps}</td>
+            </tr>
+          </tbody>
+        </table>
+      </Fragment>
+    );
+  }
 }
--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,31 +1,49 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+type InstructionType = {
+  id: number;
+  title: string;
+};
+
 export default function Instructions() {
-  let [instructions, setInstructions] = useState([{ id: 0, title: '' }]);
+  let [instructions, setInstructions] = useState([{} as InstructionType]);
+  let [error, setError] = useState(0);
+  let [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(
-    () =>
-      setInstructions([
-        { id: 1, title: 'Chilli Sin Carne' },
-        { id: 2, title: 'Broccolli Pasta' },
-        { id: 3, title: 'French Lentil Soup' },
-      ]),
-    [instructions]
-  );
+  useEffect(() => {
+    fetch('http://localhost:4000/v1/instructions')
+      .then((response) => {
+        console.log('status code is: ', response.status);
+        if (response.status !== 200) {
+          setError(response.status);
+        } else {
+          setError(0);
+        }
+        return response.json();
+      })
+      .then((json) => setInstructions(json.instructions))
+      .then(() => setIsLoaded(true));
+  }, []);
 
-  return (
-    <Fragment>
-      <h2>Choose Instruction</h2>
-      <ul>
-        {instructions.map((instruction) => (
-          <li key={instruction.id}>
-            <Link to={`/instructions/${instruction.id}`}>
-              {instruction.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </Fragment>
-  );
+  if (error !== 0) {
+    return <h2>Error: {error}</h2>;
+  } else if (isLoaded !== true) {
+    return <h2>Loading...</h2>;
+  } else {
+    return (
+      <Fragment>
+        <h2>Choose Instruction</h2>
+        <ul>
+          {instructions.map((instruction) => (
+            <li key={instruction.id}>
+              <Link to={`/instructions/${instruction.id}`}>
+                {instruction.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    );
+  }
 }
